8822403adb3094a85b336fb574fe9cd27b4df624
Baryshewww <baryshev-tm@mail.ru>
2022-11-19 13:05:43 +0300

    Добавляет выполненные задания из 3 модуля Типы данных

---
+++ lesson/module-3/task-1/typeof.js
@@ -0,0 +1,26 @@
+// Типы данных
+let stringData = 'String';
+let numberData = 7;
+let objectData = {
+    name: 'value'
+};
+let nullData = null;
+let undefinedData = undefined;
+let booleanData = true;
+let bigIntData = BigInt(50);
+let symbolData = Symbol('id');
+
+console.log('stringData', stringData, typeof stringData);
+console.log('numberData', numberData, typeof numberData);
+console.log('objectData', objectData, typeof objectData);
+console.log('nullData', nullData, typeof nullData);
+console.log('undefinedData', undefinedData, typeof undefinedData);
+console.log('booleanData', booleanData, typeof booleanData);
+console.log('bigIntData', bigIntData, typeof bigIntData);
+console.log('symbolData', symbolData, typeof symbolData);
+
+
+
+
+
+
