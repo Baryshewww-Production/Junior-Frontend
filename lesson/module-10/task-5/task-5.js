6f5d973ff0f55fbfc4abc7858ec4e580fec65559
Baryshewww <baryshev-tm@mail.ru>
2023-01-23 14:16:15 +0300

    Изменил функцию из 5 задания 10 модуля. Оптимизировал ее

--- lesson/module-10/task-5/task-5.js
+++ lesson/module-10/task-5/task-5.js
@@ -1,39 +1,47 @@
-const groceries = {
-    'Orange Juice': { price: 1.5, discount: 10 },
-    'Chocolate': { price: 2, discount: 0 },
-    'Fish': { price: 3, discount: 5 },
-    'Milk': { price: 2, discount: 20 },
-    'Tomato': { price: 1, discount: 0 },
-    'Water': { price: 0.5, discount: 0 },
-}
-
-const shoppingBagList = [
+const shoppingBag = [
     { product: 'Chocolate', quantity: 3 },
     { product: 'Orange Juice', quantity: 23 },
-    { product: 'Fish', quantity: 5 },
-    { product: 'Milk', quantity: 10 },
-    { product: 'Tomato', quantity: 6 },
-    { product: 'Water', quantity: 12 },
+    { product: 'Fish', quantity: 10 },
+    { product: 'Milk', quantity: 5 },
+    { product: 'Butter', quantity: 2 },
+    { product: 'Chips', quantity: 7 },
+    { product: 'Water', quantity: 8 },
 ];
 
-//Функция, которая считает полную стоимость всех продуктов с учетом их цены, количества и скидки на них
-const getTotalPriceOfShoppingBag = (shoppingBag) => {
-    return shoppingBag.reduce((acc, good) => {
-        //Нахожу для каждого продукта полный размер его скидки
-        let discount = good.quantity * groceries[good.product].price * (groceries[good.product].discount / 100);
-
-        //Переменная хранящая количество текущего продукта
-        let goodQuantity = good.quantity;
-
-        //Переменная хранящая стоимость текущего продукта за одну штуку
-        let goodPrice = groceries[good.product].price;
-
-        //Условие, которое проверяет есть ли скидка, и в положительном ответе считает всю стоимость текущего
-        // продукта с вычетом скидки
-        if (groceries[good.product].discount > 0) { return acc += goodQuantity * goodPrice - discount }
-        return acc += goodQuantity * goodPrice;
-    }, 0);
+const groceries = {
+    'Chocolate': { price: 1.5, discount: 10},
+    'Orange Juice': { price: 3, discount: 0},
+    'Fish': { price: 3.5, discount: 5},
+    'Milk': { price: 1, discount: 30},
+    'Butter': { price: 3, discount: 20},
+    'Chips': { price: 0.5, discount: 0},
+    'Water': { price: 1, discount: 3},
 };
 
-const total = getTotalPriceOfShoppingBag(shoppingBagList);
-console.log(total)
+function getTotalPriceOfShoppingBag(shoppingBag) {
+    // Записываю в новый массив значения переданного массива
+    const shoppingItems = Object.values(shoppingBag);
+    //Проверяю, если длина переданного массива равна нулю, значит функция вернет нуль
+    if (shoppingItems.length === 0) {
+        return 0;
+    }
+    const totalPrice = shoppingItems.reduce((acc, currentProduct) => {
+        //Делаю де-структуризацию текущего элемента в массиве.
+        const {product, quantity} = currentProduct;
+
+        //Записываю в новую переменную данные из глобального объекта по ключу, который хранит имя продукта.
+        const productObject = groceries[product];
+
+        //Делаю де-структуризацию текущего объекта, который хранит цену и скидку для текущего продукта.
+        const {price, discount} = productObject;
+        const priceWithDiscount = productObject.price - (price * discount / 100);
+        let totalProductPrice = priceWithDiscount * quantity;
+        return acc += totalProductPrice;
+    }, 0);
+
+    return totalPrice.toFixed(2);
+}
+
+const totalPrice = getTotalPriceOfShoppingBag(shoppingBag);
+console.log('totalPrice', totalPrice);
+
